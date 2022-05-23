5b3767cde34896e6d4b3ac5a85de713777c552a1
Lucas <lucasvieirasilva.lpta@gmail.com>
2022-05-23 08:41:28 -0300

    Initial commit

---
+++ src/test/java/br/com/lucas/estoqueAPI/EstoqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.estoqueAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
